4b9bbb3902956573e533e3854e43180f7c08e1f5
Linus Rengbrandt <line.rengbrandt@gmail.com>
2025-01-05 15:28:10 +0100

    added styling to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import "../styling/Footer.css";
+
 import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-      <Link to="/"><h2>FinTrackr</h2></Link>
+      <Link to="/"><h2 className="footer-logo">FinTrackr</h2></Link>
       <ul className="footer-links">
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/">Expenses</Link></li>
