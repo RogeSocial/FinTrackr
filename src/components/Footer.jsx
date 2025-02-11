50bd9f9a99d7266b45c024d5c0ae6c1cee1067a8
Linus Rengbrandt <line.rengbrandt@gmail.com>
2025-02-11 19:19:56 +0100

    added links for the expense tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
       <Link to="/"><h2 className="footer-logo">FinTrackr</h2></Link>
       <ul className="footer-links">
         <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/">Expenses</Link></li>
+        <li><Link to="/expenses">Expenses</Link></li>
         <li><Link to="/">Savings</Link></li>
         <li><Link to="/">Investments</Link></li>
       </ul>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <Link to="/" className="nav-logo">FinTrackr</Link>
         <ul className="nav-links">
           <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/">Expenses</Link></li>
+          <li><Link to="/expenses">Expenses</Link></li>
           <li><Link to="/">Savings</Link></li>
           <li><Link to="/">Investments</Link></li>
         </ul>
